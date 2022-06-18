14abb2a30e9ee2fa83a2e58b1ed661034b29dbde
Toma Iliev <iliev.toma@gmail.com>
2022-06-17 19:15:49 -0700

    warning fixed...

--- src/components/Common/Notification.js
+++ src/components/Common/Notification.js
@@ -1,14 +1,9 @@
 import { Alert, Snackbar } from "@mui/material";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import NotificationContext from "../../context/NotificationContext";
 
 const Notification = () => {
     const { notification, setNotification } = useContext(NotificationContext);
-
-    // useEffect(() => {
-
-    //     return () => setNotification(null);
-    // });
 
     return (
         <Snackbar anchorOrigin={{ vertical: 'top', horizontal: 'right' }} open={!!notification} autoHideDuration={3000} onClose={() => setNotification(null)}>
