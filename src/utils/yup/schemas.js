bad6d5e55ae2fbd13fae3d5b18484e6e12cdfc5a
Toma Iliev <iliev.toma@gmail.com>
2022-08-22 12:26:39 -0700

    validation messages updated

--- src/utils/yup/schemas.js
+++ src/utils/yup/schemas.js
@@ -7,7 +7,7 @@ const contactFormSchema = object({
     firstName: string().required('First name is required'),
     lastName: string().required('Last name is required'),
     email: string().email('Valid email is required').required(),
-    message: string().required('Message is required').min(3),
+    message: string().required('Message is required').min(3, 'Message must be 3 characters or more'),
     subscriber: boolean().default(true)
 });
 
