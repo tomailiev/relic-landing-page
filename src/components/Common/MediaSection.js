319ce806b5faec9005fe8a39369c7462d707aa5f
Toma Iliev <iliev.toma@gmail.com>
2023-06-08 18:09:34 -0700

    Revert "own animation first steps"
    This reverts commit c1e5bff31db3f22ad34e408dc3c3f03463465938.

--- src/components/Common/MediaSection.js
+++ src/components/Common/MediaSection.js
@@ -1,13 +1,15 @@
-import { Paper, Box, IconButton, } from "@mui/material";
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Grid, Paper, Typography, Slide, Box, IconButton, Button, } from "@mui/material";
+import { useEffect, useRef, useState } from "react";
 // import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 import { downloadDocs } from "../../utils/firebase/firestore-funcs";
+import { links } from "../../data/links";
+import YouTube from 'react-youtube';
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 // import CircleIcon from '@mui/icons-material/Circle';
 import './MediaSection.css';
-import VideoGrid from "./VideoGrid";
 
 
 const MediaSection = () => {
@@ -28,14 +30,14 @@ const MediaSection = () => {
             })
     }, []);
 
-    useEffect(() => {
-        if (videos.length) {
-            intervalRef.current = setInterval(() => {
-                setId(prev => prev + 1 === videos.length ? 0 : ++prev);
-            }, 7000);
-        }
-        return () => clearInterval(intervalRef.current);
-    }, [videos.length])
+    // useEffect(() => {
+    //     if (videos.length) {
+    //         intervalRef.current = setInterval(() => {
+    //             setId(prev => prev + 1 === videos.length ? 0 : ++prev);
+    //         }, 5000);
+    //     }
+    //     return () => clearInterval(intervalRef.current);
+    // }, [videos.length])
 
     function changeVid(vidIndex) {
         switchInterval(true);
@@ -43,19 +45,19 @@ const MediaSection = () => {
     }
 
     function switchInterval(sw) {
-        if (sw === true) {
-            clearInterval(intervalRef.current);
-            setTimer(false);
-            return;
-        }
+        // if (sw === true) {
+        //     clearInterval(intervalRef.current);
+        //     setTimer(false);
+        //     return;
+        // }
 
-        if (timer) {
-            clearInterval(intervalRef.current);
-        } else {
-            intervalRef.current = setInterval(() => {
-                setId(prev => prev + 1 === videos.length ? 0 : ++prev);
-            }, 5000);
-        }
+        // if (timer) {
+        //     clearInterval(intervalRef.current);
+        // } else {
+        //     intervalRef.current = setInterval(() => {
+        //         setId(prev => prev + 1 === videos.length ? 0 : ++prev);
+        //     }, 5000);
+        // }
         setTimer(prev => !prev);
     }
 
@@ -63,9 +65,61 @@ const MediaSection = () => {
         <>
             <Paper sx={{ my: 2, p: 1, }}>
                 <Box position={'relative'} ref={containerRef} overflow={'hidden'}>
-                    {videos.map((vid, i) => (
-                        i === id && <VideoGrid key={vid.youtubeId} video={vid} switchInterval={switchInterval} />
-                    ))}
+                    {/* {videos.map((vid, i) => ( */}
+                        {/* <Slide key={vid.youtubeId} timeout={750} exit={false} in={id === i} direction={'left'} container={containerRef.current} mountOnEnter unmountOnExit> */}
+                            <Grid container spacing={2} justifyContent="center" my={4}  className={timer ? 'hidden-left' : 'shown'}>
+                                <Grid item md={6} sm={8}>
+                                    <YouTube
+                                        videoId={videos[0]?.youtubeId}
+                                        opts={{ height: '300px', width: '100%', }}
+                                        onPlay={switchInterval}
+                                    />
+                                </Grid>
+                                <Grid item md={6} textAlign={'center'}>
+                                    <Typography variant="h5" mb={2} mx={2}>
+                                        {videos[0]?.title}
+                                    </Typography>
+                                    <Button variant="contained" sx={{ background: '#f60000', '&:hover': { background: '#a90000' } }} endIcon={<SubscriptionsIcon />} href={links.ytSubscribe} target="_blank">
+                                        {'Subscribe'}
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            <Grid container spacing={2} justifyContent="center" my={4} className={timer ? 'shown' : 'hidden-right'}>
+                                <Grid item md={6} sm={8}>
+                                    <YouTube
+                                        videoId={videos[1]?.youtubeId}
+                                        opts={{ height: '300px', width: '100%', }}
+                                        onPlay={switchInterval}
+                                    />
+                                </Grid>
+                                <Grid item md={6} textAlign={'center'}>
+                                    <Typography variant="h5" mb={2} mx={2}>
+                                        {videos[1]?.title}
+                                    </Typography>
+                                    <Button variant="contained" sx={{ background: '#f60000', '&:hover': { background: '#a90000' } }} endIcon={<SubscriptionsIcon />} href={links.ytSubscribe} target="_blank">
+                                        {'Subscribe'}
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                            <Grid container spacing={2} justifyContent="center" my={4} className="hidden-right">
+                                <Grid item md={6} sm={8}>
+                                    <YouTube
+                                        videoId={videos[2]?.youtubeId}
+                                        opts={{ height: '300px', width: '100%', }}
+                                        onPlay={switchInterval}
+                                    />
+                                </Grid>
+                                <Grid item md={6} textAlign={'center'}>
+                                    <Typography variant="h5" mb={2} mx={2}>
+                                        {videos[2]?.title}
+                                    </Typography>
+                                    <Button variant="contained" sx={{ background: '#f60000', '&:hover': { background: '#a90000' } }} endIcon={<SubscriptionsIcon />} href={links.ytSubscribe} target="_blank">
+                                        {'Subscribe'}
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        {/* </Slide> */}
+                    {/* // ))} */}
                 </Box>
                 <Box display={'flex'} justifyContent={'center'}>
                     <IconButton
