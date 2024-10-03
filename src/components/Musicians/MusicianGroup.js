d4746ff5be9014ba7ded798030fd64031bc24d88
Toma Iliev <iliev.toma@gmail.com>
2024-10-03 11:20:31 -0400

    added chair option

--- src/components/Musicians/MusicianGroup.js
+++ src/components/Musicians/MusicianGroup.js
@@ -9,8 +9,8 @@ const MusicianGroup = ({ section }) => {
             {
                 section?.length
                     ? <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                        {section?.map(({ name, pic, bio, title, id, founder }) => {
-                            return <MusicianLI key={id} name={name} picUrl={pic} bio={bio} title={title} founder={founder} />
+                        {section?.map(({ name, pic, bio, title, id, founder, chair }) => {
+                            return <MusicianLI key={id} name={name} picUrl={pic} bio={bio} title={title} founder={founder} chair={chair} />
                         })}
                     </List>
                     : <MusicianSkeleton />
