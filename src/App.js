0c30d491a575d1c6c28e7bcc58d714f555e14c7a
Toma Iliev <iliev.toma@gmail.com>
2025-01-16 14:43:33 -0500

    fix/pdf-worker/attempt3

--- src/App.js
+++ src/App.js
@@ -35,10 +35,8 @@ import Host from './components/Support/Host';
 import { pdfjs } from 'react-pdf';
 
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  'pdfjs-dist/build/pdf.worker.min.mjs',
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
+
 
 function App() {
 
