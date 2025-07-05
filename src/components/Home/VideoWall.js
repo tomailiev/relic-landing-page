2c1af1953b727e55dcef96ab6a4ea04da941803d
Toma Iliev <iliev.toma@gmail.com>
2025-07-04 22:50:51 -0700

    feature/videos/home+dialog-bg

--- src/components/Home/VideoWall.js
+++ src/components/Home/VideoWall.js
@@ -1,38 +1,43 @@
-import { Paper, Box, IconButton, useTheme, useMediaQuery, Tooltip, } from "@mui/material";
-import { useEffect, useRef, useState } from "react";
+import { Paper, Box, useTheme, useMediaQuery, Grid, Button, Typography, } from "@mui/material";
+import { useEffect, useState } from "react";
 // import HorizontalRuleIcon from '@mui/icons-material/HorizontalRule';
 import { downloadDocsV2 } from "../../utils/firebase/firestore-funcs";
 // import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 // import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 // import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
-import VideoItem from "./VideoItem";
-import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+// import VideoItem from "./VideoItem";
+// import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 // import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
-import PauseIcon from '@mui/icons-material/Pause';
-import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
-import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+// import PauseIcon from '@mui/icons-material/Pause';
+// import NavigateBeforeIcon from '@mui/icons-material/NavigateBefore';
+// import NavigateNextIcon from '@mui/icons-material/NavigateNext';
+import YouTube from "react-youtube";
+// import { links } from "../../data/links";
+// import { Subscriptions } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 // import CircleIcon from '@mui/icons-material/Circle';
 
 
 const VideoWall = () => {
-    const [videos, setVideos] = useState([]);
-    const [index, setIndex] = useState(0);
-    const [timer, setTimer] = useState(true);
-    const [activePlayer, setActivePlayer] = useState(null);
-    const [buttonsDisabled, setButtonsDisabled] = useState(false);
-    let intervalRef = useRef(null);
+    const [video, setVideo] = useState([]);
+    // const [index, setIndex] = useState(0);
+    // const [timer, setTimer] = useState(true);
+    // const [activePlayer, setActivePlayer] = useState(null);
+    // const [buttonsDisabled, setButtonsDisabled] = useState(false);
+    // let intervalRef = useRef(null);
 
     const theme = useTheme();
     const lgMatch = useMediaQuery(theme.breakpoints.up('md'));
 
     useEffect(() => {
         downloadDocsV2('videos', [
-            { value: ['featured', '!=', 0], type: 'condition' },
-            { value: ['featured', 'desc'], type: 'sorting' },
-            { value: [3], type: 'limit' }
+            { value: ['featured', '==', 5], type: 'condition' },
+            // { value: ['featured', 'desc'], type: 'sorting' },
+            { value: [1], type: 'limit' }
         ])
             .then((docs) => {
-                setVideos(docs);
+
+                setVideo(docs[0]);
             })
             .catch(e => {
                 console.error('not found');
@@ -40,60 +45,60 @@ const VideoWall = () => {
             })
     }, []);
 
-    useEffect(() => {
-        if (videos.length) {
-            intervalRef.current = setInterval(() => {
-                setIndex(prev => prev + 1 === videos.length ? 0 : ++prev);
-            }, 5000);
-        }
-        return () => clearInterval(intervalRef.current);
-    }, [videos.length])
+    // useEffect(() => {
+    //     if (videos.length) {
+    //         intervalRef.current = setInterval(() => {
+    //             setIndex(prev => prev + 1 === videos.length ? 0 : ++prev);
+    //         }, 5000);
+    //     }
+    //     return () => clearInterval(intervalRef.current);
+    // }, [videos.length])
 
-    function playVideo(e) {
-        switchInterval(true);
-        setActivePlayer(e.target);
-    }
+    // function playVideo(e) {
+    //     switchInterval(true);
+    //     setActivePlayer(e.target);
+    // }
 
 
-    function switchActive(direction) {
-        if (activePlayer) {
-            activePlayer.stopVideo();
-            setActivePlayer(null);
-        }
-        setButtonsDisabled(true);
-        setTimeout(() => {
-            setButtonsDisabled(false);
-        }, 900)
-        setIndex(prev => prev + direction === videos.length
-            ? 0 : prev + direction < 0
-                ? videos.length - 1
-                : prev + direction);
-        clearInterval(intervalRef.current);
-        setTimer(false);
-    }
+    // function switchActive(direction) {
+    //     if (activePlayer) {
+    //         activePlayer.stopVideo();
+    //         setActivePlayer(null);
+    //     }
+    //     setButtonsDisabled(true);
+    //     setTimeout(() => {
+    //         setButtonsDisabled(false);
+    //     }, 900)
+    //     setIndex(prev => prev + direction === videos.length
+    //         ? 0 : prev + direction < 0
+    //             ? videos.length - 1
+    //             : prev + direction);
+    //     clearInterval(intervalRef.current);
+    //     setTimer(false);
+    // }
 
-    function switchInterval(sw) {
+    // function switchInterval(sw) {
 
-        if (sw) {
-            clearInterval(intervalRef.current);
-            setTimer(false);
-        } else {
-            if (activePlayer) {
-                activePlayer.stopVideo();
-                setActivePlayer(null);
-            }
-            intervalRef.current = setInterval(() => {
-                setIndex(prev => prev + 1 === videos.length ? 0 : ++prev);
-            }, 5000);
-            setTimer(true);
-        }
-    }
+    //     if (sw) {
+    //         clearInterval(intervalRef.current);
+    //         setTimer(false);
+    //     } else {
+    //         if (activePlayer) {
+    //             activePlayer.stopVideo();
+    //             setActivePlayer(null);
+    //         }
+    //         intervalRef.current = setInterval(() => {
+    //             setIndex(prev => prev + 1 === videos.length ? 0 : ++prev);
+    //         }, 5000);
+    //         setTimer(true);
+    //     }
+    // }
 
     return (
         <>
             <Paper sx={{ my: 2, px: 1, }}>
                 <Box position={'relative'} overflow={'hidden'} height={lgMatch ? '370px' : '540px'} >
-                    {videos.map((vid, i, arr) => (
+                    {/* {videos.map((vid, i, arr) => (
                         <VideoItem
                             key={vid.youtubeId}
                             video={vid}
@@ -103,9 +108,27 @@ const VideoWall = () => {
                             switchInterval={switchInterval}
                             length={arr.length}
                         />
-                    ))}
+                    ))} */}
+                    <Grid container spacing={2} justifyContent="center" my={4} >
+                        <Grid item md={6} sm={8} xs={12}>
+                            <YouTube
+                                videoId={video.youtubeId}
+                                opts={{ height: '300px', width: '100%', }}
+                            />
+                        </Grid>
+                        <Grid item md={6} textAlign={'center'}>
+                            <Typography variant="h5" mb={2} mx={2}>
+                                {video.title}
+                            </Typography>
+                            <Link to={'/media/videos'}>
+                                <Button variant="outlined">
+                                    {'More videos'}
+                                </Button>
+                            </Link>
+                        </Grid>
+                    </Grid>
                 </Box>
-                <Box display={'flex'} justifyContent={'center'}>
+                {/* <Box display={'flex'} justifyContent={'center'}>
                     <Tooltip title={'previous video'}>
                         <IconButton
                             size="large"
@@ -135,8 +158,8 @@ const VideoWall = () => {
                             <NavigateNextIcon />
                         </IconButton>
                     </Tooltip>
-                </Box>
-            </Paper>
+                </Box> */}
+            </Paper >
         </>
     );
 };
--- src/components/Videos/Videos.js
+++ src/components/Videos/Videos.js
@@ -1,13 +1,10 @@
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { downloadDocsV2 } from "../../utils/firebase/firestore-funcs";
-import { Box, Button, Card, CardContent, Container, FormControl, Grid, IconButton, InputLabel, MenuItem, Paper, Select, Typography } from "@mui/material";
-import { PlayCircleFilled, PlayCircleOutline } from "@mui/icons-material";
-import DialogContext from "../../context/DialogContext";
-import YouTube from "react-youtube";
+import { Container, FormControl, Grid, InputLabel, MenuItem, Select, Typography } from "@mui/material";
+import VideoItem from "./VideoItem";
 
 const Videos = () => {
 
-    const { setDialog } = useContext(DialogContext)
     const [videoCategory, setVideoCategory] = useState('live');
     const [videos, setVideos] = useState([]);
 
@@ -21,10 +18,6 @@ const Videos = () => {
 
     function handleSelectChange(event) {
         setVideoCategory(event.target.value)
-    }
-
-    function setYoutubeDialog(video) {
-        setDialog({ type: 'video', title: video.title, component: <YouTube videoId={video.youtubeId} opts={{ playerVars: { autoplay: 1 }, width: '100%', height: '370px', }} />})
     }
 
     return (
@@ -57,95 +50,7 @@ const Videos = () => {
             </Container>
             {videos?.length && <Grid container spacing={6} my={3}>
                 {videos.map(video => {
-                    return <Grid key={video.id} item xs={12} sm={6} md={4} display={'flex'}>
-                        <Paper elevation={2} sx={{ display: 'flex', flexDirection: 'column', flex: 1 }}>
-                            <Card
-                                sx={{
-                                    flexGrow: 1,
-                                    position: 'relative',
-                                    overflow: 'hidden',
-                                }}
-                            >
-                                <Box
-                                    sx={{
-                                        position: 'relative',
-                                        width: '100%',
-                                        pt: '56.25%', // 16:9 aspect ratio
-                                        overflow: 'hidden',
-                                    }}
-                                >
-                                    <Box
-                                        component="img"
-                                        src={video.thumbnail}
-                                        alt={video.title}
-                                        sx={{
-                                            position: 'absolute',
-                                            width: '100%',
-                                            height: '100%',
-                                            top: '50%',
-                                            left: '50%',
-                                            objectFit: 'cover',
-                                            transform: 'translate(-50%, -50%)',
-                                            transition: 'all 400ms ease',
-                                            '.MuiCard-root:hover &': {
-                                                width: '110%',
-                                                height: '110%'
-                                            },
-                                        }}
-                                    />
-
-                                    {/* Play icon overlay */}
-                                    <IconButton
-                                        disableRipple
-                                        color="secondary"
-                                        sx={{
-                                            position: 'absolute',
-                                            top: '50%',
-                                            left: '50%',
-                                            transform: 'translate(-50%, -50%)',
-                                            width: '20%',
-                                            p: 0,
-                                        }}
-                                    >
-                                        {/* Outline icon (default) */}
-                                        <PlayCircleOutline
-                                            sx={{
-                                                width: '100%',
-                                                height: 'auto',
-                                                position: 'absolute',
-                                                transition: 'opacity 400ms ease, transform 400ms ease',
-                                                opacity: 1,
-                                                '.MuiCard-root:hover &': {
-                                                    opacity: 0,
-                                                },
-                                            }}
-                                        />
-                                        {/* Filled icon (on hover) */}
-                                        <PlayCircleFilled
-                                            sx={{
-                                                width: '100%',
-                                                height: 'auto',
-                                                position: 'absolute',
-                                                transition: 'opacity 400ms ease, transform 400ms ease',
-                                                opacity: 0,
-                                                '.MuiCard-root:hover &': {
-                                                    opacity: 1,
-                                                },
-                                            }}
-                                        />
-                                    </IconButton>
-                                </Box>
-
-                                {/* ─────────── Title ─────────── */}
-                                <CardContent>
-                                    <Typography variant="body1" fontWeight="bold">
-                                        {video.title}
-                                    </Typography>
-                                </CardContent>
-                                <Button sx={{ width: '100%', height: '100%', left: 0, top: 0, position: 'absolute' }} onClick={() => setYoutubeDialog(video)} />
-                            </Card>
-                        </Paper>
-                    </Grid>
+                    return <VideoItem video={video} />
                 })}
             </Grid>}
         </Container>
