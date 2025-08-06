d8ab8de8a9436c4af66b3d96d254f8350cb2679e
Toma Iliev <iliev.toma@gmail.com>
2025-08-06 07:33:49 -0700

    fix/parallax-scroll-bg-bug

--- src/components/Home/BannerParallax.js
+++ src/components/Home/BannerParallax.js
@@ -34,8 +34,9 @@ const BannerParallax = ({ bgPic }) => {
             <Box
                 sx={{
                     position: 'absolute',
-                    top: 0,
-                    left: 0,
+                    left: '50%',
+                    top: '50%',
+                    transform: 'translate(-50%, -50%)',
                     width: '100%',
                     height: '100%',
                     backgroundImage: `url(${bgPic})`,
