e6d02e9d222fe293f5b2b63b6313621259e342c6
Toma Iliev <iliev.toma@gmail.com>
2025-09-22 00:43:21 -0400

    feature/eb-widget-fallbacks

--- src/components/Events/CheckoutDialog.js
+++ src/components/Events/CheckoutDialog.js
@@ -1,21 +1,44 @@
 import { useContext, useEffect, useRef } from "react";
 import NotificationContext from "../../context/NotificationContext";
+import LoadingContext from "../../context/LoadingContext";
 
 const CheckoutDialog = ({ eventId }) => {
   const containerRef = useRef(null);
   const { setNotification } = useContext(NotificationContext);
+  const {setLoading} = useContext(LoadingContext);
 
   useEffect(() => {
     if (!window.EBWidgets) return;
-
+    setLoading(true);
     window.EBWidgets.createWidget({
       widgetType: "checkout",
       eventId,
       iframeContainerId: "eventbrite-widget-container",
-      // iframeContainerHeight: 600,
+      iframeContainerHeight: 700,
       onOrderComplete: () => setNotification({ type: 'success', message: 'Thank you for your order!' }),
     });
-  }, [eventId, setNotification]);
+
+    const container = document.getElementById("eventbrite-widget-container");
+    const observer = new MutationObserver(() => {
+      const iframe = container.querySelector("iframe");
+      if (iframe) {
+        iframe.addEventListener("load", () => {
+          setLoading(false);
+          console.log("Eventbrite widget iframe loaded");
+        });
+        iframe.addEventListener('', () => {
+          setLoading(false);
+          console.log('iframe errored out');
+
+        })
+        observer.disconnect();
+      }
+    });
+
+    observer.observe(container, { childList: true });
+
+    return () => observer.disconnect();
+  }, [eventId, setNotification, setLoading]);
 
   return <div id="eventbrite-widget-container" ref={containerRef} />;
 };
